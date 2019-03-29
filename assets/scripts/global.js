538724be2b32aea9240ad396db3b6d780153e20c
Sergiu Mare <mare.sergiu@yahoo.com>
2019-03-29 21:10:34 +0000

    feature/project-set-up: refactoring

--- assets/scripts/global.js
+++ assets/scripts/global.js
@@ -5,56 +5,56 @@
  */
 
 // project namespace
-window.temp || (window.temp = {});
+window.lab || (window.lab = {});
 
 (function ($) {
 	$(document).ready(function () {
 
-		$.extend(true, temp, {
+		$.extend(true, lab, {
 			cache: toolkit.cache,
 			config: toolkit.config,
 			helpers: toolkit.helpers
 		});
 
-		// custom config        
-		temp.config.breakpoints.MEDIUM_WIDTH = 1200;
-		temp.config.breakpoints.X_MEDIUM_WIDTH = 1024;
-		temp.config.breakpoints.SMALL_WIDTH = 768;
+		// custom config
+		lab.config.breakpoints = {
+			MEDIUM_WIDTH: 1200,
+			X_MEDIUM_WIDTH: 1024,
+			SMALL_WIDTH: 768
+		}
 
 		// custom delays
-		temp.cache.timer = {};
-		temp.cache.timer.veryFast = 150;
-		temp.cache.timer.fast = 300;
-		temp.cache.timer.moderated = 500;
+		lab.cache.timer = {
+			veryFast: 150,
+			fast: 300,
+			moderated: 500
+		};
 
 		// custom cache
-		temp.cache.$header = $('#site-header');
-		temp.cache.$footer = $('#site-footer');
-		temp.cache.$main = $('#main');				
+		lab.cache.$header = $('#site-header');
+		lab.cache.$footer = $('#site-footer');
+		lab.cache.$main = $('#main');
 
 		// if the height of the body is smaller then the page make the bodies height to strech to fit the screen 
-		temp.helpers.isContentScrolable();
+		lab.helpers.isContentScrolable();
 
 		// detect if js in enable
-		temp.cache.$html.removeClass('no-js').addClass('js');
-		
+		lab.cache.$html.removeClass('no-js').addClass('js');
+
 
 		// animate the elements that are in the viewport on load
-		temp.helpers.animateElemInView();
-		
+		lab.helpers.animateElemInView();
+
 
 		// window events
 		// -------------
 		// avoiding the momentum scrolling on mobile and trigger animations
-		if (temp.helpers.deviceWithTouch()) {
-			temp.cache.$window.on({				
-				'scroll': temp.helpers.animateElemInView
+		if (lab.helpers.deviceWithTouch()) {
+			lab.cache.$window.on({
+				'scroll': lab.helpers.animateElemInView
 			});
-		}		
-
-		// scroll on desktop
-		if (!temp.helpers.deviceWithTouch()) {
-			toolkit.scrollTimer.push(temp.helpers.animateElemInView);			
+		} else {
+			toolkit.scrollTimer.push(lab.helpers.animateElemInView);
 		}
 	});
 })(jQuery);
